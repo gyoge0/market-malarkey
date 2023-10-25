cc917042a05a022107b656f8d1479275f3dbede5
Yogesh Thambidurai <gyogesh2015@outlook.com>
2023-10-25 18:09:41 -0400

    feat: add header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,48 @@
+<div id="outer">
+  <h1>Market Malarkey</h1>
+
+  <div id="group">
+    <span>Your Revenue: <strong>320</strong></span>
+    <span>|</span>
+    <span>Opponent Revenue: <strong>570</strong></span>
+  </div>
+</div>
+
+<style>
+  #outer {
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: flex-start;
+    align-items: center;
+    background-color: mediumpurple;
+
+    width: 100%;
+    padding-top: 0.25rem;
+    padding-bottom: 0.25rem;
+    padding-left: 1.5rem;
+    padding-right: 1.5rem;
+    border-radius: 3rem;
+  }
+
+  h1 {
+    font-family: "Playfair Display Variable", sans-serif;
+    font-weight: bold;
+    font-size: 3em;
+    flex-grow: 0;
+  }
+
+  #group {
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: flex-end;
+    align-items: center;
+    flex-grow: 1;
+    font-size: 2em;
+    gap: 1rem;
+    font-family: "Verdana", sans-serif;
+  }
+  strong {
+    font-weight: bold;
+    font-size: 1.25em;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
+<script lang="ts">
+  import Header from "$lib/components/Header.svelte";
+</script>
+
 <main>
-  <h1>Hello World</h1>
+  <Header />
 </main>
 
 <style>
@@ -8,11 +12,10 @@
     height: 100%;
     display: flex;
     flex-flow: column nowrap;
-    justify-content: center;
+    justify-content: flex-start;
     align-items: center;
-  }
 
-  h1 {
-    font-family: sans-serif;
+    padding: 2rem;
+    background: #181818;
   }
 </style>
